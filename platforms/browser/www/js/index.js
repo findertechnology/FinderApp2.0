b73f93ff6ccd55ba861a4e1f172a73d1013a935e
findertechnology <findertechnology@gmail.com>
2018-12-12 13:51:16 +0100

    Commit 12-12-2018

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -32,15 +32,37 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+       /* var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        console.log('Received Event: ' + id);*/
     }
 };
 
 app.initialize();
+document.getElementById("img1").src = "img/finderscores.png";
+
+function seleccion(elem){
+	alert("Hola: "+ elem);
+	var act = document.getElementById(elem).src;
+	var part = act.split(".");
+	if(part.split("-")[1] == null){
+		//document.getElementById(elem).src = part[0]+ "-selected."+ part[1];
+	}
+}
+
+function deseleccion(elem){
+	var act = document.getElementById(elem).src;
+	var part = act.split("-");
+	if(part[1] != null){
+		var part2 = part[1].split(".");
+		//document.getElementById(elem).src = part[0]+ "."+ part2[1];
+	}
+	
+}
+
+
